4243fbd6dc9bb1275c59a13f293909841d6d414d
MartinWallmark <wallmarkmartin@gmail.com>
2022-04-11 21:20:32 +0200

    Generator changes

--- Assets/Script/Generator.cs
+++ Assets/Script/Generator.cs
@@ -5,20 +5,58 @@ using UnityEngine;
 public class Generator : MonoBehaviour
 {
     [SerializeField] private float fuel;
+    [SerializeField] private float secondDelay;
+    [SerializeField] private Light fuelLevelIndicator;
+    private float timer;
+
+    private Color fullTankColor = new Color(0, 133, 0, 255);
+    private Color halfEmptyTankColor = new Color(188, 174, 0, 255);
+    private Color emptyTankColor = new Color(188, 0, 0, 255);
+
+    private float maxFuel;
+    private bool isEmpty;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxFuel = fuel;
+        fuelLevelIndicator.color = fullTankColor;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isEmpty)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= secondDelay)
+            {
+                DecreaseFuelLevel();
+                timer = 0;
+            }
+        }
+
+        if (fuel == maxFuel/2)
+        {
+            fuelLevelIndicator.color = halfEmptyTankColor;
+        }
         
     }
 
-    private void Refill()
+    public void Refill()
     {
         fuel = 100;
+        fuelLevelIndicator.color = fullTankColor;
+    }
+
+    private void DecreaseFuelLevel()
+    {
+        fuel--;
+        if(fuel <= 0)
+        {
+            isEmpty = true;
+            fuelLevelIndicator.color = emptyTankColor;
+        }
     }
 }
