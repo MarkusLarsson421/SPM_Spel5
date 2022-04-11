ff502a5726edef74d7237c292c1ad7e51c61ce88
Markus Larsson <82539179+MarkusLarsson421@users.noreply.github.com>
2022-04-11 18:16:06 +0200

    Worked on flash light.

---
+++ Assets/Script/FlashLight.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    [SerializeField][Range(1, 10)]private int range;
+    [SerializeField]private bool toggleOnOff = false;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
