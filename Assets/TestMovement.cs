7773e85cda2a1128a2f8da4bf7a89b1378b8f01d
Agzzel <sterneraxel@protonmail.com>
2022-04-12 17:41:11 +0200

    Kommenterat ut en del i testmovement

--- Assets/TestMovement.cs
+++ Assets/TestMovement.cs
@@ -5,28 +5,33 @@ using UnityEngine.InputSystem;
 
 public class TestMovement : MonoBehaviour
 {
-    PlayerControls controls;
+    /*PlayerControls controls;
     Vector2 moveF;
     Vector2 moveB;
     Vector2 moveL;
-    Vector2 moveR;
+    Vector2 moveR;*/
 
     private void Awake()
     {
-        controls = new PlayerControls();
+        //controls = new PlayerControls();
 
-        controls.Gameplay.MoveForward.performed += ctx => moveF = ctx.ReadValue<Vector2>();
+        /*controls.Gameplay.MoveForward.performed += ctx => moveF = ctx.ReadValue<Vector2>();
         controls.Gameplay.MoveForward.canceled += ctx => moveF = Vector2.zero;
-        /*controls.Gameplay.MoveForward.performed += ctx => moveB = ctx.ReadValue<Vector2>();
+        controls.Gameplay.MoveForward.performed += ctx => moveB = ctx.ReadValue<Vector2>();
         controls.Gameplay.MoveForward.performed += ctx => moveL = ctx.ReadValue<Vector2>();
         controls.Gameplay.MoveForward.performed += ctx => moveR = ctx.ReadValue<Vector2>();*/
     }
 
     private void Update()
     {
-        Vector2 mF = new Vector2(moveF.x, moveF.y) * Time.deltaTime;
+        /*Vector2 mF = new Vector2(moveF.x, moveF.y) * Time.deltaTime;
         transform.Translate(mF, Space.World);
-        /*Vector2 mB = new Vector2(moveB.x, moveF.y) * Time.deltaTime;
+        Vector2 mB = new Vector2(moveB.x, moveF.y) * Time.deltaTime;
         transform.Translate(mB, Space.World);*/
+
+        var gamepad = Gamepad.current;
+        Vector2 move = gamepad.leftStick.ReadValue() * Time.deltaTime;
+        transform.Translate(move, Space.World);
+        
     }
 }
