d1f071199679f2d1f1b37990d61ebc1812b2ad76
MartinWallmark <wallmarkmartin@gmail.com>
2022-04-13 22:19:10 +0200

    småstartat med ett craftingsystem

--- Assets/Script/CraftingSystem.cs
+++ Assets/Script/CraftingSystem.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 //Martin Wallmark
 public class CraftingSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int scrapAmountNeeded;
+    private GameObject player;
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void damageUpgrade()
     {
-        
+        //Ska göra så pistolen gör mer skada
+    }
+
+    private void flashLightUpgrade()
+    {
+        //ska göra så ficklampans batterie räcker längre(eller nåt)
     }
 }
