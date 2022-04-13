4dbb02357aa8b7845ec75d2ce3da5c1366f295b1
Markus Larsson <82539179+MarkusLarsson421@users.noreply.github.com>
2022-04-13 16:21:21 +0200

    Update SingleDoor.cs

--- Assets/Script/SingleDoor.cs
+++ Assets/Script/SingleDoor.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SingleDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private bool blocked = true;
+	private bool doorOpen;
+	private Collider trigger;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start()
+	{
+		//transform.GetComponent<Collision>() //Reference to trigger collision box.
+	}
+
+	void Update()
+	{
+		//TODO
+		//Raycast, see if it hits the door.
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			RaycastHit hit;
+			/*if(Physics.Raycast())
+			{
+				ToggleState();
+			}*/
+			
+		}
+	}
+
+	public void ToggleState()
+	{
+		SetState(!doorOpen);
+	}
+	
+	private void SetState(bool desiredState){
+		if(desiredState && !blocked){
+			// Open door.
+			gameObject.transform.GetChild(0).gameObject.SetActive(true);
+			doorOpen = true;
+		}else{
+			// Close door.
+			gameObject.transform.GetChild(0).gameObject.SetActive(false);
+			doorOpen = false;
+		}
+	}
 }
