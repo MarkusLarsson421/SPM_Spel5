af43355c42589df17a073c8cb9f380f04b3c783d
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-04-14 14:59:34 +0200

    kommentar

--- Assets/Script/CanvasHandler.cs
+++ Assets/Script/CanvasHandler.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class CanvasHandler : MonoBehaviour
+public class CanvasHandler : MonoBehaviour // @Khaled Alraas
 {
-    [SerializeField] private Button tryAgainButton;
+    [SerializeField] private Button tryAgainButton; 
 
     // Start is called before the first frame update
     void Start()
