d84fe4a091bbc8d6ae441a85851830ebe8a76f22
khalosh74 <alrassk74@gmail.com>
2022-04-20 18:57:43 +0200

    build + menu +menuscript

--- Assets/Script/UI_Main_Menu.cs
+++ Assets/Script/UI_Main_Menu.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_Main_Menu : MonoBehaviour
 {
+    [SerializeField] private Button StartButton;
+    [SerializeField] private int sceneToIndex;
+    [SerializeField] private Button OptionsButton;
+    [SerializeField] private Button ExitButton;
+    [SerializeField] private GameObject ExitConfirmationCanvas;
+    [SerializeField] private Button YesExitButton;
+    [SerializeField] private Button NoExitButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,22 @@ public class UI_Main_Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        StartButton.onClick.AddListener(LoadNextLevel);
+        ExitButton.onClick.AddListener(Exit);
+
+    }
+    void ChangeCanvas()
+    {
+        ExitConfirmationCanvas.SetActive(true);
+        LoadNextLevel();
+    }
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(sceneToIndex);
+    }
+    private void Exit()
+    {
+        Debug.Log("exit");
+        Application.Quit();
     }
 }
