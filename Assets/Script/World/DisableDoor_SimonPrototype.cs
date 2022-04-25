f699605dedfe7416aba18e56d94b8bf903d913f0
SimonH <simon.h.oscarson@hotmail.se>
2022-04-25 11:31:27 +0200

    MOSTADVANCEDSCRIPTEVER

--- Assets/Script/World/DisableDoor_SimonPrototype.cs
+++ Assets/Script/World/DisableDoor_SimonPrototype.cs
@@ -6,16 +6,7 @@ public class DisableDoor_SimonPrototype : MonoBehaviour
 {
     public GameObject door1;
     public GameObject door2;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 
     public void DisableDoors()
     {
