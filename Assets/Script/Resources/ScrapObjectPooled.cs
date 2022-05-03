625ee883aab525de0f01aba578af6a9c67f5bbb1
Agzzel <sterneraxel@protonmail.com>
2022-05-03 11:40:59 +0200

    Spawn för scrap funkar nu

--- Assets/Script/Resources/ScrapObjectPooled.cs
+++ Assets/Script/Resources/ScrapObjectPooled.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*@Author Axel Sterner
+ * Klass som instansierar scrap-objekt ur poolen. Läggs på en prefab som agerar spawner
+ */
 public class ScrapObjectPooled : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnScrap();
+        }
+    }
+
+    private void SpawnScrap()
+    {
+        var scrap = ScrapPool.Instance.Get();
+        scrap.transform.position = transform.position;
+        scrap.transform.rotation = transform.rotation;
+        scrap.gameObject.SetActive(true);
     }
 }
