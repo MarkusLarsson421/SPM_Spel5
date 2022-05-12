75f1f2b66f4bbba8ba41150ea401bda6bfd477d3
SimonH <simon.h.oscarson@hotmail.se>
2022-05-12 13:54:43 +0200

    fixad scenen och fixat wincondition!
    Co-Authored-By: MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>

--- Assets/Script/World/WinGame_SimonPrototype.cs
+++ Assets/Script/World/WinGame_SimonPrototype.cs
@@ -1,17 +1,50 @@
 using UnityEngine;
+using System.Collections;
 
 public class WinGame_SimonPrototype : MonoBehaviour
 {
     public ResourceManager rm;
     public CanvasHandler ch;
     private int scrapsInCar;
-    private int scrapsNeededToFixCar = 25;
+    private int scrapsNeededToFixCar = 1;
+    private bool isClicked;
 
+
+    private void Update()
+    {
+        if (isClicked)
+        {
+            AddScrapsToCar();
+            isClicked = false;
+        }
+    }
+    public void CarInteraction()
+    {
+        isClicked = true;
+    }
     public void AddScrapsToCar()
     {
+        wait();
+        Debug.Log("test132");
        scrapsInCar += rm.Get(ResourceManager.ItemType.Scrap);
       
        rm.SetTotal(ResourceManager.ItemType.Scrap, 0);
+        Debug.Log("rm scraps = " + rm.Get(ResourceManager.ItemType.Scrap) + " scrapsInCar = " + scrapsInCar);
+        if (scrapsInCar >= scrapsNeededToFixCar)
+        {
+            ch.ChangeCanvasToDeathCanvas();
+            Debug.Log("u won the game");
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1f);
+
+        Debug.Log("test132");
+        scrapsInCar += rm.Get(ResourceManager.ItemType.Scrap);
+
+        rm.SetTotal(ResourceManager.ItemType.Scrap, 0);
         Debug.Log("rm scraps = " + rm.Get(ResourceManager.ItemType.Scrap) + " scrapsInCar = " + scrapsInCar);
         if (scrapsInCar >= scrapsNeededToFixCar)
         {
