eb2595a8a4b0f3feb9335656699ae40377dc5b2a
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-05-12 15:34:16 +0200

    ui

---
+++ Assets/Script/World/CarPopOutText.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using EventCallbacks;
+
+public class CarPopOutText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject playerOne;
+    private GameObject playerTwo;
+    private float distance1;
+    private float distance2;
+    [SerializeField] private Transform playerTransform;
+    float distance;
+
+    private void Update()
+    {
+        ClosestPlayer();
+        distance = Vector3.Distance(transform.position, playerTransform.position);
+
+        if (distance < 5)
+        {
+            PlayeIsNearTheCarEvent playerGetHitByZombie = new PlayeIsNearTheCarEvent();
+            EventSystem.Current.FireEvent(playerGetHitByZombie);
+        }
+    }
+    private void ClosestPlayer()
+    {
+        playerOne = GameObject.FindGameObjectWithTag("Player1");
+        if (playerOne != null)
+        {
+            playerTransform = playerOne.transform;
+            playerTwo = GameObject.FindGameObjectWithTag("Player2");
+            if (playerTwo != null)
+            {
+                distance1 = Vector3.Distance(transform.position, playerOne.transform.position);
+                distance2 = Vector3.Distance(transform.position, playerTwo.transform.position);
+                if (distance2 < distance1)
+                {
+                    playerTransform = playerTwo.transform;
+                }
+            }
+        }
+    }
+
+
+}
