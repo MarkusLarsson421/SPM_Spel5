17c6327842609da8847ab8deb92d2b4342c82456
MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
2022-05-17 16:49:40 +0200

    städat i craftingscriptet

--- Assets/Script/Resources/CraftingSystem.cs
+++ Assets/Script/Resources/CraftingSystem.cs
@@ -10,18 +10,16 @@ using UnityEngine.EventSystems;
 
 public class CraftingSystem : MonoBehaviour
 {
-    public ResourceManager rm;
-
-    [SerializeField] private int scrapAmountNeeded;
+    
     [SerializeField] private GameObject canvas;
     [SerializeField] private Font font;
     [SerializeField] private EventSystem eventSystem;
+    [SerializeField] private Interactable inter;
+    [SerializeField] public string currentPlayerTag;
     private EventSystem currentPlayerEventSystem;
     
-    //Används för att kunna identifiera vilken spelare det är som interagerar med craftingbordet
-    [SerializeField] private Interactable inter; 
-
-    [SerializeField] public string currentPlayerTag;
+    
+    
     private Text infoText;
     private bool isToggled;
 
@@ -33,10 +31,7 @@ public class CraftingSystem : MonoBehaviour
     private List<string> flashLightUpgradedPlayers = new List<string>();
 
     private Canvas currentCanvas;
-    private GameObject damageUpgrade;
-    private GameObject magazineUpgrade;
-    private GameObject flashlightUpgrade;
-    private GameObject cancelButton;
+   
 
 
     public GameObject interactingPlayer;
@@ -54,19 +49,7 @@ public class CraftingSystem : MonoBehaviour
         infoText.font = font;
         infoText.fontSize = 32;
         infoText.enabled = false;
-       
-
-        damageUpgrade = GameObject.Find("DamageUpgrade");
-        magazineUpgrade = GameObject.Find("MagazineUpgrade");
-        flashlightUpgrade = GameObject.Find("fireRateUpgrade");
-        cancelButton = GameObject.Find("CancelButton");
-        
-        
-        damageUpgrade.SetActive(false);
-        magazineUpgrade.SetActive(false);
-        flashlightUpgrade.SetActive(false);
-        cancelButton.SetActive(false);
-        
+      
     }
 
      void Update()
@@ -114,14 +97,14 @@ public class CraftingSystem : MonoBehaviour
     public void ToggleCraftingBench()
     {
         
-        //toggleButtons();
+        
         if (!isToggled)
         {
             
             Cursor.lockState = CursorLockMode.None;
             isToggled = true;
             infoText.enabled = true;
-            eventSystem.SetSelectedGameObject(cancelButton);
+            
             infoText.text = "Craft here! \n" + "\nUpgrade damage: 2 Batteries, 2 Scraps \n" + "\nUpgrade magazine: 1 Battery, 3 Scraps \n" + "\nUpgrade flashlight: 3 Batteries, 1 Scrap";
             
         }
@@ -156,12 +139,9 @@ public class CraftingSystem : MonoBehaviour
 
                 damageUpgradedPlayers.Add(inter.interactingGameObject.transform.parent.tag);
                 UpdateInfoText("GotUpgrade");
-                Debug.Log("GOT UPGRADE");
             }
             else
             {
-                Debug.Log("Too few batteries pal");
-                Debug.Log(inter.interactingGameObject.transform.parent.tag);
                 UpdateInfoText("NotEnoughItems");
             }
             isButtonClicked = true;
@@ -177,7 +157,7 @@ public class CraftingSystem : MonoBehaviour
             //Gör så att vapnets magasin kan ha fler patroner
             if (MagazineUpgradedPlayers.Contains(inter.interactingGameObject.transform.parent.tag))
             {
-                //ToggleCraftingBench();
+                
                 UpdateInfoText("AlreadyHasUpgrade");
             }
             else if (inter.interactingGameObject.GetComponentInChildren<ResourceManager>().Get(ResourceManager.ItemType.Battery) >= 1 && inter.interactingGameObject.GetComponentInChildren<ResourceManager>().Get(ResourceManager.ItemType.Scrap) >= 3)
@@ -186,16 +166,16 @@ public class CraftingSystem : MonoBehaviour
                 inter.interactingGameObject.GetComponentInChildren<ResourceManager>().Offset(ResourceManager.ItemType.Scrap, -3);
                 inter.interactingGameObject.GetComponentInChildren<Weapon>().SetMagCapacity(12);
                 MagazineUpgradedPlayers.Add(inter.interactingGameObject.transform.parent.tag);
-                //ToggleCraftingBench();
+                
                 UpdateInfoText("GotUpgrade");
 
             }
             else
             {
-                //ToggleCraftingBench();
+                
                 UpdateInfoText("NotEnoughItems");
             }
-            //ToggleCraftingBench();
+            
             Debug.Log("wahwah");
             isButtonClicked = true;
         }
