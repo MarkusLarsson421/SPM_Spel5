65b83adb883670109ff1ad83c5106a3ece7a42cd
khalosh74 <alrassk74@gmail.com>
2022-05-17 17:58:06 +0200

    Ny meny

--- Assets/Script/UI/MainMenu/MenuController.cs
+++ Assets/Script/UI/MainMenu/MenuController.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour //Khaled Alraas
 {
+    [Header("Valume Settings")]
+    [SerializeField] private TMP_Text valumeTextValue = null;
+    [SerializeField] private Slider valumeSlider= null;
+    [SerializeField] private float defualtValume = 0.5f;
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     [Header("Level to Load")]
     [SerializeField] private string sceneNameToLoad;
 
@@ -21,5 +30,32 @@ public class MenuController : MonoBehaviour //Khaled Alraas
     {
         yield return null;
         SceneManager.LoadScene(sceneNameToLoad);
+    }
+    public void SetValume(float valume)
+    {
+        AudioListener.volume = valume;
+        valumeTextValue.text = valume.ToString("0.0");
+    }
+    public void ValumeApply()
+    {
+        PlayerPrefs.SetFloat("masterValume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string menuType)
+    {
+        if(menuType == "Audio")
+        {
+            AudioListener.volume = defualtValume;
+            valumeSlider.value = defualtValume;
+            valumeTextValue.text = defualtValume.ToString("0.0");
+            ValumeApply();
+        }
+    }
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
