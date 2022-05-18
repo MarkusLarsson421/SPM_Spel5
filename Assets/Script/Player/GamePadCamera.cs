4ad36eb7b9b7d66090087288fa68c7431fca7ea8
MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
2022-05-18 10:06:50 +0200

    fixat i kamerascriptet så den funkar för både horisontell och vertikal splitscreen

--- Assets/Script/Player/GamePadCamera.cs
+++ Assets/Script/Player/GamePadCamera.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-//testscript för att få till handkontrollern
 //Martin Wallmark
 public class GamePadCamera : MonoBehaviour
 {
@@ -10,26 +9,23 @@ public class GamePadCamera : MonoBehaviour
     
     private InputSystem input;
     private Vector2 look;
+    private Transform player;
+    
     private float xRotation = 0f;
-
     private float xAxis;
     private float yAxis;
     private float smallRotationInput = 0.15f;
 
-    private Transform player;
+    private bool isSplitScreenVertical;
+
+    
     // Start is called before the first frame update
     void Awake()
     {
         player = transform.parent;
-        if (transform.parent.tag.Equals("Player1"))
+        if(transform.parent.tag.Equals("Player2"))
         {
-            //GetComponent<Camera>().rect = new Rect(0f, 0.5f, 1f, 0.5f);
-        }
-        else if(transform.parent.tag.Equals("Player2"))
-        {
-            //transform.position = new Vector2(0, -1);
-            GameObject.FindWithTag("Player1").GetComponentInChildren<Camera>().rect = new Rect(0f, 0.5f, 1f, 0.5f);
-            GetComponent<Camera>().rect = new Rect(0f, 0f, 1f, 0.5f);
+            SetUpSplitScreen();  
         }
 
         input = new InputSystem();
@@ -64,15 +60,36 @@ public class GamePadCamera : MonoBehaviour
 
 
         xRotation -= yAxis;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 70f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         player.Rotate(Vector3.up * xAxis);
     }
 
+    private void SetUpSplitScreen()
+    {
+        if (isSplitScreenVertical)
+        {
+            GameObject.FindWithTag("Player1").GetComponentInChildren<Camera>().rect = new Rect(0f, 0f, 0.5f, 1f);
+            GetComponent<Camera>().rect = new Rect(0.5f, 0f, 0.5f, 1f);
+        }
+        else
+        {
+            GameObject.FindWithTag("Player1").GetComponentInChildren<Camera>().rect = new Rect(0f, 0.5f, 1f, 0.5f);
+            GetComponent<Camera>().rect = new Rect(0f, 0f, 1f, 0.5f);
+        }
+        
+        
+    }
+
     public void SetSensitivity(float nSensitivty)
     {
         sensitity = nSensitivty;
+    }
+
+    public void SetVerticalSplitScreen(bool isSplitScreenVertical)
+    {
+        this.isSplitScreenVertical = isSplitScreenVertical;
     }
 
     private void OnEnable()
