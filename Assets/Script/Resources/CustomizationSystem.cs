c1134d3b5b63e0f7566178a3750a7660e1bb51d9
MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
2022-05-26 15:47:16 +0200

    nya hathar till customization!

--- Assets/Script/Resources/CustomizationSystem.cs
+++ Assets/Script/Resources/CustomizationSystem.cs
@@ -26,8 +26,12 @@ public class CustomizationSystem : MonoBehaviour
     private GameObject playerOneCustomizationButtons, playerTwoCustomizationButtons;
     private GameObject playerOneHatButton, playerTwoHatButton;
     private GameObject playerOneNoHatButton, playerTwoNoHatButton;
+    private GameObject playerOneMysteryHatButton, playerTwoMysteryHatButton;
+    private GameObject playerOneMilitaryHatButton, playerTwoMilitaryHatButton;
    
     private GameObject playerOneHat, playerTwoHat;
+    private GameObject playerOneMysteryHat, playerTwoMysteryHat;
+    private GameObject playerOneMilitaryHat, playerTwoMilitaryHat;
     private Canvas playerOneCanvas, playerTwoCanvas;
     private EventSystem playerOneEventSystem, playerTwoEventSystem;
 
@@ -98,7 +102,7 @@ public class CustomizationSystem : MonoBehaviour
     }
 
     /**
-     * Finds the canvas inside the interacting player and activates the buttons
+     * Hittar canvasen hos spelaren som interagerar och customizationbordet och hittar alla saker som behövs och därefter cachas det så den inte behöver göra tunga uträkingar mer än en gång.
      */
     private void SetCurrentCanvas()
     {
@@ -107,23 +111,35 @@ public class CustomizationSystem : MonoBehaviour
         {
             playerOneCanvas = interactingPlayer.GetComponentInChildren<Canvas>();
             playerOneHat = interactingPlayer.transform.Find("Hat").gameObject;
+            playerOneMysteryHat = interactingPlayer.transform.Find("MysteryHat").gameObject;
+            playerOneMilitaryHat = interactingPlayer.transform.Find("MilitaryHat").gameObject;
             playerOneCustomizationButtons = playerOneCanvas.gameObject.transform.Find("CustomizationButtons").gameObject;
             playerOneCustomizationButtons.SetActive(true);
             playerOneHatButton = playerOneCustomizationButtons.transform.Find("HatButton").gameObject;
             playerOneNoHatButton = playerOneCustomizationButtons.transform.Find("NoHatButton").gameObject;
+            playerOneMilitaryHatButton = playerOneCustomizationButtons.transform.Find("MilitaryHatButton").gameObject;
+            playerOneMysteryHatButton = playerOneCustomizationButtons.transform.Find("MysteryHatButton").gameObject;
             playerOneNoHatButton.GetComponent<Button>().onClick.AddListener(delegate { DisableHat(); });
             playerOneHatButton.GetComponent<Button>().onClick.AddListener(delegate { ActivateHat(); });
+            playerOneMysteryHatButton.GetComponent<Button>().onClick.AddListener(delegate { ActivateMysteryHat(); });
+            playerOneMilitaryHatButton.GetComponent<Button>().onClick.AddListener(delegate { ActivateMilitaryHat(); });
         }
         else
         {
             playerTwoCanvas = interactingPlayer.GetComponentInChildren<Canvas>();
             playerTwoHat = interactingPlayer.transform.Find("Hat").gameObject;
+            playerOneMysteryHat = interactingPlayer.transform.Find("MysteryHat").gameObject;
+            playerOneMilitaryHat = interactingPlayer.transform.Find("MilitaryHat").gameObject;
             playerTwoCustomizationButtons = playerTwoCanvas.gameObject.transform.Find("CustomizationButtons").gameObject;
             playerTwoCustomizationButtons.SetActive(true);
             playerTwoHatButton = playerTwoCustomizationButtons.transform.Find("HatButton").gameObject;
             playerTwoNoHatButton = playerTwoCustomizationButtons.transform.Find("NoHatButton").gameObject;
+            playerTwoMilitaryHatButton = playerTwoCustomizationButtons.transform.Find("MilitaryHatButton").gameObject;
+            playerTwoMysteryHatButton = playerTwoCustomizationButtons.transform.Find("MysteryHatButton").gameObject;
             playerTwoNoHatButton.GetComponent<Button>().onClick.AddListener(delegate { DisableHat(); });
             playerTwoHatButton.GetComponent<Button>().onClick.AddListener(delegate { ActivateHat(); });
+            playerTwoMysteryHatButton.GetComponent<Button>().onClick.AddListener(delegate { ActivateMysteryHat(); });
+            playerTwoMilitaryHatButton.GetComponent<Button>().onClick.AddListener(delegate { ActivateMilitaryHat(); });
         }
         interactedPlayers.Add(interactingPlayer.tag);
 
@@ -151,10 +167,14 @@ public class CustomizationSystem : MonoBehaviour
         if (interactingPlayer.tag.Equals("Player1"))
         {
             playerOneHat.SetActive(true);
+            playerOneMysteryHat.SetActive(false);
+            playerOneMilitaryHat.SetActive(false);
         }
         else
         {
             playerTwoHat.SetActive(true);
+            playerOneMysteryHat.SetActive(false);
+            playerOneMilitaryHat.SetActive(false);
         }
         
     }
@@ -164,12 +184,48 @@ public class CustomizationSystem : MonoBehaviour
         if (interactingPlayer.tag.Equals("Player1"))
         {
             playerOneHat.SetActive(false);
+            playerOneMysteryHat.SetActive(false);
+            playerOneMilitaryHat.SetActive(false);
         }
         else
         {
             playerTwoHat.SetActive(false);
+            playerOneMysteryHat.SetActive(false);
+            playerOneMilitaryHat.SetActive(false);
         }
         
+    }
+
+    private void ActivateMysteryHat()
+    {
+        if (interactingPlayer.tag.Equals("Player1"))
+        {
+            playerOneMysteryHat.SetActive(true);
+            playerOneHat.SetActive(false);
+            playerOneMilitaryHat.SetActive(false);
+        }
+        else
+        {
+            playerTwoMysteryHat.SetActive(true);
+            playerTwoHat.SetActive(false);
+            playerTwoMilitaryHat.SetActive(false);
+        }
+    }
+
+    private void ActivateMilitaryHat()
+    {
+        if (interactingPlayer.tag.Equals("Player1"))
+        {
+            playerOneMilitaryHat.SetActive(true);
+            playerOneMysteryHat.SetActive(false);
+            playerOneHat.SetActive(false);
+        }
+        else
+        {
+            playerTwoMilitaryHat.SetActive(true);
+            playerTwoMysteryHat.SetActive(false);
+            playerTwoHat.SetActive(false);
+        }
     }
 
     private void ActivateInteractingPlayer()
