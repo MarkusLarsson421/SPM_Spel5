caa1e1ca563dca7b863eabfe508c1742adfb79de
khalosh74 <alrassk74@gmail.com>
2022-06-02 11:23:27 +0200

    Win cavas score text fix2

--- Assets/Script/World/ScoreSystem.cs
+++ Assets/Script/World/ScoreSystem.cs
@@ -8,11 +8,15 @@ public class ScoreSystem : MonoBehaviour
 {
     [SerializeField] private InreaseWaveListener increasedWave;
     [SerializeField] private TMP_Text waveText;
+    [SerializeField] private TMP_Text winWaveText;
     [SerializeField] private ZombieDeathListener zombieKilled;
     [SerializeField] private TMP_Text zombieKilledText;
+    [SerializeField] private TMP_Text winZombieKilledText;
     [SerializeField] private HeadShootListener headShots;
     [SerializeField] private TMP_Text headShotText;
+    [SerializeField] private TMP_Text winHeadShotText;
     [SerializeField] private TMP_Text timeTakenText;
+    [SerializeField] private TMP_Text winTimeTakenText;
     private float timeValue;
 
 
@@ -32,20 +36,24 @@ public class ScoreSystem : MonoBehaviour
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
         timeTakenText.text = "time: "+ minutes + "." + seconds;
+        winTimeTakenText.text = "time: " + minutes + "." + seconds;
     }
 
     public void DisplayZombieWave()
     {
         waveText.text = "Wave: " +  increasedWave.GetCurrentWave().ToString();
+        winWaveText.text = "Wave: " + increasedWave.GetCurrentWave().ToString();
     }
 
     public void DisplayZombiesKilled()
     {
         zombieKilledText.text = "Zombie Killed: " + zombieKilled.GetDeathCounter().ToString();
+        winZombieKilledText.text = "Zombie Killed: " + zombieKilled.GetDeathCounter().ToString();
     }
     public void DisplayHeadShots()
     {
         headShotText.text = "Head Shots: " + headShots.GetHeadShotCounter().ToString();
+        winHeadShotText.text = "Head Shots: " + headShots.GetHeadShotCounter().ToString();
     }
     public void UpdateValues()
     {
