22ddc0d366a155fb02881795e7522c6558e6028f
khalosh74 <62815679+khalosh74@users.noreply.github.com>
2022-06-02 19:46:25 +0200

    win canvas fix

--- Assets/Script/World/ScoreSystem.cs
+++ Assets/Script/World/ScoreSystem.cs
@@ -6,25 +6,29 @@ using EventCallbacks;
 
 public class ScoreSystem : MonoBehaviour
 {
+    [Header("WaveText")]
     [SerializeField] private InreaseWaveListener increasedWave;
     [SerializeField] private TMP_Text waveText;
     [SerializeField] private TMP_Text waveText2;
     [SerializeField] private TMP_Text winWaveText;
+    [SerializeField] private TMP_Text winWaveText2;
     [SerializeField] private ZombieDeathListener zombieKilled;
-    
+    [Header("ZombieKilled")]
     [SerializeField] private TMP_Text zombieKilledText;
     [SerializeField] private TMP_Text zombieKilledText2;
     [SerializeField] private TMP_Text winZombieKilledText;
+    [SerializeField] private TMP_Text winZombieKilledText2;
     [Header("HeadShots")]
     [SerializeField] private HeadShootListener headShots;
     [SerializeField] private TMP_Text headShotText;
     [SerializeField] private TMP_Text winHeadShotText;
     [SerializeField] private TMP_Text headShotTextp2;
-    //[SerializeField] private TMP_Text winHeadShotTextp2;
+    [SerializeField] private TMP_Text winHeadShotTextp2;
     [Header("Time")]
     [SerializeField] private TMP_Text timeTakenText;
     [SerializeField] private TMP_Text timeTakenText2;
     [SerializeField] private TMP_Text winTimeTakenText;
+    [SerializeField] private TMP_Text winTimeTakenText2;
     private float timeValue;
     private bool isUpdatingTime = true;
 
@@ -51,6 +55,8 @@ public class ScoreSystem : MonoBehaviour
         timeTakenText.text = "time: "+ minutes + "." + seconds;
         timeTakenText2.text = "time: " + minutes + "." + seconds;
         winTimeTakenText.text = "time: " + minutes + "." + seconds;
+        winTimeTakenText2.text = "time: " + minutes + "." + seconds;
+
     }
 
     public void DisplayZombieWave()
@@ -59,6 +65,7 @@ public class ScoreSystem : MonoBehaviour
         waveText.text = "Waves: " +  increasedWave.GetCurrentWave().ToString();
         waveText2.text = "Waves: " + increasedWave.GetCurrentWave().ToString();
         winWaveText.text = "Waves: " + increasedWave.GetCurrentWave().ToString();
+        winWaveText2.text = "Waves: " + increasedWave.GetCurrentWave().ToString();
     }
 
     public void DisplayZombiesKilled()
@@ -73,10 +80,12 @@ public class ScoreSystem : MonoBehaviour
         if (!zombieKilled.CheckPlayer2())
         {
             zombieKilledText2.text = "Zombies Killed: 0";
+            winZombieKilledText2.text = "Zombies Killed: 0";
         }
         zombieKilledText2.text = "Zombies Killed: " + zombieKilled.GetDeathCounter2().ToString();
+        winZombieKilledText2.text = "Zombies Killed: " + zombieKilled.GetDeathCounter2().ToString();
     }
-    
+
     public void DisplayHeadShots()
     {
         headShotText.text = "Headshots: " + headShots.GetHeadShotCounter().ToString();
@@ -84,10 +93,13 @@ public class ScoreSystem : MonoBehaviour
     }
     public void DisplayHeadShots2()
     {
-        if (!headShots.CheckPlayer2()) { headShotTextp2.text = "Headshots: "; }
+        if (!headShots.CheckPlayer2()) { 
+            headShotTextp2.text = "Headshots: 0";
+            winHeadShotTextp2.text = "Headshots: 0";
+        }
 
         headShotTextp2.text = "Headshots: " + headShots.GetHeadShotCounter2().ToString();
-        //winHeadShotTextp2.text = "Headshots: " + headShots.GetHeadShotCounter2().ToString();
+        winHeadShotTextp2.text = "Headshots: " + headShots.GetHeadShotCounter2().ToString();
     }
     public void UpdateValues()
     {
